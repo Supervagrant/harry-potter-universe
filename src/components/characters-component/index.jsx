d46d41b01194ea56b34cd6aec8e95e45de59740f
Klindziuk <kklindziuk@gmail.com>
2025-02-17 21:48:57 +0100

    Move code for character list to his own file

--- src/components/characters-component/index.jsx
+++ src/components/characters-component/index.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState, useMemo, useCallback } from "react";
+import CharactersList from "../characters-list";
 import debounce from "lodash/debounce";
 import "./style.css";
 
@@ -70,53 +71,7 @@ const CharactersComponent = () => {
             placeholder="search caracter by name..."
             onChange={onSearchChange}
           />
-          <div className="character-list">
-            {filteredCharacters.map((character) => {
-              console.log(character);
-              return (
-                <div key={character.id} className="character">
-                  {character.image && (
-                    <img
-                      className="character__image"
-                      src={character.image}
-                      alt={`${character.name} portrait`}
-                      width="160px"
-                      height="auto"
-                      onError={(e) => (e.target.style.display = "none")}
-                    />
-                  )}
-                  <div className="character__data">
-                    {character.name && (
-                      <p>
-                        <span>Name:</span> {character.name}
-                      </p>
-                    )}
-                    {character.dateOfBirth && (
-                      <p>
-                        <span>Date of birth:</span> {character.dateOfBirth}
-                      </p>
-                    )}
-                    {character.house && (
-                      <p>
-                        <span>Hogwarts house:</span> {character.house}
-                      </p>
-                    )}
-                    {character.patronus && (
-                      <p>
-                        <span>Patronus:</span> {character.patronus}
-                      </p>
-                    )}
-                    {character.wand.core && (
-                      <p>
-                        <span>Magic wand:</span> {character.wand.core} and{" "}
-                        {character.wand.wood}
-                      </p>
-                    )}
-                  </div>
-                </div>
-              );
-            })}
-          </div>
+          <CharactersList characters={filteredCharacters} />
         </div>
       ) : (
         <p>No characters available</p>
